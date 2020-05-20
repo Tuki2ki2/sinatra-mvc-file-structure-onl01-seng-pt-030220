3e45510b9cd996964517b939e6f280127d077260
Erik Prenzlin <eprenzlin@gmail.com>
2020-05-20 20:33:14 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,20 @@
 class Model
 
+  class dog
+  attr_accessor :name,:age, :breed
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize
+  @name = name
+  @breed = breed
+  @age = age
+  end
+
+  end
+
 end
